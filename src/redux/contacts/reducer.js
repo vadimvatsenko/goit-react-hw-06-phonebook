6e3c10a65fcd0a02eebc0dfe5cb3afb8d8e07ef7
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2023-01-20 21:06:47 +0200

    20.01.2023 21:06

--- src/redux/contacts/reducer.js
+++ src/redux/contacts/reducer.js
@@ -1,44 +1,53 @@
 import { combineReducers } from "redux";
 
-// const contactsInitialState = {
-//   contacts: [],
-//   filters: ''
-// };
-const contactsInitialState = []
+const contactsInitialState = {
+  contacts: [],
+  filters: ''
+};
+// const contactsInitialState = []
 
 
-console.log(contactsInitialState)
 
-const contactsReducer = (state = contactsInitialState, action) => {
-  // export const rootReducer = (state = contactsInitialState, action) => {
+export const rootReducer = (state = contactsInitialState, action) => {
   switch (action.type) {
     case "contacts/addContacts":
-      return [...state, action.payload];
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload],
+      };
     case 'contacts/delContacts':
-    return state.filter(contact => contact.id !== action.payload)
+    return {
+        ...state,
+        contacts: state.contacts.filter(contact => contact.id !== action.payload),
+      };
+    case "filter/filterContacts":
+      return {
+        ...state,
+        contacts: state.contacts.filter(contact => contact.name === action.payload),
+      }
     default:
       return state;
   }
 };
 
-const filtersInitialState = ''
+// const filtersInitialState = ''
 
-const filterReducer = (state = filtersInitialState, action) => {
-  switch (action.type) {
-    case 'filter/filterContacts':
-      return {
-        ...state,
-        contacts: state.filter(contact => contact.id !== action.payload),
-      }
-    default:
-      return state
-  }
-}
+// const filterReducer = (state = filtersInitialState, action) => {
+//   switch (action.type) {
+//     case 'filter/filterContacts':
+//       return {
+//         // ...state,
+//         contacts: contactsInitialState.filter(contact => contact.id !== action.payload),
+//       }
+//     default:
+//       return state
+//   }
+// }
 
-export const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filters: filterReducer,
-});
+// export const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+//   filters: filterReducer,
+// });
 
 //сдесь важно, contscts и ашдеукі уже не используются сверху, каждому редусеру свой стейт
 
